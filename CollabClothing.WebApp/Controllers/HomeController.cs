438145561c9ce1cb78e0e65c5fdd75d366bee677
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2022-05-20 15:32:12 +0700

    fix bug

--- CollabClothing.WebApp/Controllers/HomeController.cs
+++ CollabClothing.WebApp/Controllers/HomeController.cs
@@ -24,6 +24,7 @@ namespace CollabClothing.WebApp.Controllers
         public async Task<IActionResult> Index()
         {
             var banner = await _bannerApiClient.GetAll();
+
             return View();
         }
 
