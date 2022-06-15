86e203ba8a88f238cd8d83cf733d71d5cfa95d47
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2022-06-15 13:30:58 +0700

    done update cart ajax

--- CollabClothing.WebApp/wwwroot/js/cart.js
+++ CollabClothing.WebApp/wwwroot/js/cart.js
@@ -1,12 +1,56 @@
 ﻿var CartJS = function () {
     this.initialize = function () {
         loadCart()
+        EventListener()
+    }
+    function EventListenerBase(id, quantity) {
+        $.ajax({
+            type: "POST",
+            url: '/Cart/UpdateCart',
+            data: {
+                id: id,
+                quantity: quantity
+            },
+            success: function (response) {
+                const count = response.reduce((item, object) => {
+                    return item + object.quanitty;
+                }, 0);
+                loadCart();//load lai cart update price
+                $('#count-cart-quantity').text(count);
+            },
+            error: function (err) {
+                console.log(err)
+            },
+        })
+    }
+    function EventListener() {
+        $('body').on('click', '.btn_asc', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            const quantity = parseInt($('#txt_quantity_' + id).val()) + 1;
+            EventListenerBase(id, quantity);
+        });
+        $('body').on('click', '.btn_desc', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            const quantity = parseInt($('#txt_quantity_' + id).val()) - 1;
+            EventListenerBase(id, quantity);
+        });
+        $('body').on('click', '.btn_remove', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            EventListenerBase(id, 0);
+        });
     }
     function loadCart() {
         $.ajax({
             type: "GET",
             url: '/Cart/GetCart',
             success: function (response) {
+                if (response.length === 0) {
+                    $('#cart-content').hide()
+                    $('#cart-empty').css('display', 'block')
+                }
                 var html = '';
                 var total = 0;
                 var count = 0;
@@ -18,12 +62,12 @@
                         + "<div class=\"row-in text-muted\">" + item.name + "</div>"
                         + "<div class=\"row-in\">" + item.type + "</div>"
                         + " <div class=\"row-in\">Size: S</div>"
-                        + " <div class=\"row-in\">Brand: "+item.brandName+"</div>"
+                        + " <div class=\"row-in\">Brand: " + item.brandName + "</div>"
                         + "</div>"
                         + "<div class=\"col\" style=\"margin : auto\">"
-                        + "<a href=\"#\" class=\"a-link\">-</a><input value=\"" + item.quantity + "\" class=\"border-quantity-cart\"/><a href=\"#\" class=\"a-link\">+</a>"
+                        + "<a href=\"#\" class=\"a-link btn_desc\" data-id=\"" + item.productId + "\">-</a><input id=\"txt_quantity_" + item.productId + "\" value=\"" + item.quantity + "\" class=\"border-quantity-cart\"/><a href=\"#\" class=\"a-link btn_asc\" data-id=\"" + item.productId + "\">+</a>"
                         + "</div>"
-                        + " <div class=\"col\" style=\"margin : auto\"> " + item.price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' }) + " <a class=\"close\">&#10005;</a></div>"
+                        + " <div class=\"col\" style=\"margin : auto\"> " + item.price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' }) + " <a  class=\"close btn_remove\" data-id=\"" + item.productId + "\">&#10005;</a></div>"
                         + "</div>"
                         + "</div>"
                     total += item.price * item.quantity
